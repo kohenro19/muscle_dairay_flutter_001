fca55a548445bad2c28e6d589d7213dc02992236
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2023-06-19 08:12:16 +0900

    update the ui

--- lib/note_card.dart
+++ lib/note_card.dart
@@ -18,20 +18,48 @@ class _NotecardState extends State<Notecard> {
   @override
   Widget build(BuildContext context) {
 
-    return Container(
-          height: 130,
-          width: MediaQuery.of(context).size.width-20,
-          decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.only(
-              topRight: Radius.circular(30),
-              topLeft: Radius.circular(30),
-              bottomRight: Radius.circular(30),
-              bottomLeft: Radius.circular(30)
-            )
+    // return Container(
+    //       height: 130,
+    //       width: MediaQuery.of(context).size.width-20,
+    //       decoration: BoxDecoration(
+    //         // image: AssetImage(),
+    //         boxShadow: [
+    //           BoxShadow(
+    //             // blurRadius: 40,
+    //             offset: Offset(8, 10)
+    //           )
+    //         ],
+    //         color: Colors.red,
+    //         borderRadius: BorderRadius.only(
+    //           topRight: Radius.circular(30),
+    //           topLeft: Radius.circular(30),
+    //           bottomRight: Radius.circular(30),
+    //           bottomLeft: Radius.circular(30)
+    //         )
+    //       ),
+    //     );
+    return ClipRRect(
+      borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+      child: Stack(
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              AspectRatio(
+                aspectRatio: 2,
+                child: Image.asset(
+                  "images/happiness.jpg",
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Container(
+                height: 10,
+                color: Colors.red,
+              )
+            ]
           ),
-        );
-        
+           ],
+      ),
+    );       
   }
 
 }
