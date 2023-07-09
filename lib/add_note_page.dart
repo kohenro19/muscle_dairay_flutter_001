864d2e0e624b198969bbfe1f24328494d49316df
kohenro19 <39029637+kohenro19@users.noreply.github.com>
2023-07-09 16:26:32 +0900

    add the dropdown menu

--- lib/add_note_page.dart
+++ lib/add_note_page.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
 
-class AddNote extends StatelessWidget {
+class AddNote extends StatefulWidget {
   const AddNote({Key? key}) : super(key: key);
 
   @override
+  State<AddNote> createState() => _AddNoteState();
+}
+
+class _AddNoteState extends State<AddNote> {
+
+    Map<String, List<String>> _dropDownMenu = {
+    'Study': ['Math', 'Englsih', 'Japanese'],
+    'Workout': ['Shoulder', 'Chest', 'Back'],
+    'Coding': ['Flutter', 'Python', 'C#']
+  };
+
+   String? _selectedKey;
+   String? _selectedItem;
+  //  late String newValue;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
+        return Scaffold(
       appBar: AppBar(
         centerTitle: true,
         leading: GestureDetector( // leadingは、戻りの矢印
@@ -18,8 +32,80 @@ class AddNote extends StatelessWidget {
       style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
       )
       ),
-      body: Center(child: Text("Muscle Diary")),
+      body: Column(
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: <Widget>[
+              Text(
+                'What did you do?',
+                style: TextStyle(fontSize: 24),
+              ),
+              DropdownButton<String>(
+                value: _selectedKey,
+                icon: Icon(Icons.arrow_drop_down),
+                iconSize: 30,
+                elevation: 16,
+                style: TextStyle(fontSize: 20, color: Colors.black),
+                underline: Container(
+                  height: 2,
+                  color: Colors.grey,
+                ),
+
+                onChanged: (newValue) {
+                  setState(() {
+                    _selectedKey = newValue!;
+                    _selectedItem = _dropDownMenu[_selectedKey]![0];
+                  });
+                },
+                items: _dropDownMenu.keys
+                    .map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ],
+          ),
+          _selectedKey != null
+              ? Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Text(
+                      'Which one?',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                    DropdownButton<String>(
+                      value: _selectedItem,
+                      icon: Icon(Icons.arrow_drop_down),
+                      iconSize: 30,
+                      elevation: 16,
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                      underline: Container(
+                        height: 2,
+                        color: Colors.grey,
+                      ),
+                      onChanged: (newValue) {
+                        setState(() {
+                          _selectedItem = newValue!;
+                        });
+                      },
+                      items: _dropDownMenu[_selectedKey]
+                          ?.map<DropdownMenuItem<String>>((String value) {
+                        return DropdownMenuItem<String>(
+                          value: value,
+                          child: Text(value),
+                        );
+                      }).toList(),
+                    ),
+                  ],
+                )
+              : Container(),
+        ],
+      ),
     );
+  
     
   }
 }
